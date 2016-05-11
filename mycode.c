04afe19c0806dc0890f8cefc33ac4f0ece822dd9
Vysakh Venugopal <vysakh@hifx.co.in>
2016-05-11 12:00:17 +0530

    Removing float function

--- mycode.c
+++ mycode.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 int sum(int,int);
-float sum(float,float);
 double sum(double,double);
 void main()
 {
@@ -10,10 +9,6 @@ void main()
 int sum(int a, int b)
 {
   //adding a+b;
-}
-float sum(float a, float b)
-{
- // adding float a and b
 }
 double sum(double a, double b)
 {
