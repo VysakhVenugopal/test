87988de0d79be5d2597e61f3e38fc8c4152841f4
Vysakh Venugopal <vysakh@hifx.co.in>
2016-05-11 11:54:29 +0530

    initial code

---
+++ mycode.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int sum(int,int);
+float sum(float,float);
+void main()
+{
+   //goes some code here...
+}
+
+int sum(int a, int b)
+{
+  //adding a+b;
+}
+float sum(float a, float b)
+{
+ // adding float a and b
+}
