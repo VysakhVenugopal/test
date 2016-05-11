96ed9e485f714ae107c89bbe5e7d80b41645e705
Vysakh Venugopal <vysakh@hifx.co.in>
2016-05-11 12:21:51 +0530

    first commit

---
+++ mynewcode.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int sum(int,int);
+float sum(float,float);
+double sum(double,double);
+void main()
+{
+   //goes some code here...
+}
+
+int sum(int a, int b)
+{
+  //adding a+b;
+}
+double sum(double a, double b)
+{
+ // adding two doubles
+}
+float sum(float q, float w)
+{
+  //adding two floats
+}
+
